f25fb38451f574a6014c3cffb4969d34ca62c79f
Jaswin Reddy Mandavyapuram <152372672+LightY-01@users.noreply.github.com>
2026-07-13 14:43:24 +0530

    Solved one-way and bidirectional roads issue

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pyrosm import OSM
 
-osm = OSM("mumbai.osm.pbf")
+osm = OSM("data/mumbai.osm.pbf")
 nodes, edges = osm.get_network(nodes=True,network_type="driving")
 
 # get unique node ids from osm
@@ -16,14 +16,40 @@ id_map = {node_id: i for i, node_id in enumerate(osm_node_ids)}
 active_nodes = nodes[nodes["id"].isin(osm_node_ids)].copy()
 active_nodes['node_id'] = active_nodes['id'].map(id_map)
 nodes_out = active_nodes[['node_id','lat','lon']].sort_values("node_id")
-nodes_out.to_csv("nodes.csv",index=False)
+nodes_out.to_csv("data/nodes.csv",index=False)
 
-edges_out = edges[['u', 'v', 'length']].copy()
-edges_out['source_id'] = edges_out['u'].map(id_map)
-edges_out['target_id'] = edges_out['v'].map(id_map)
-edges_out.rename(columns={'length': 'distance'}, inplace=True)
-edges_out = edges_out[['source_id', 'target_id', 'distance']]
-edges_out.to_csv("edges.csv",index=False)
+# Correctly handle directionality based on OSM's 'oneway' rules
+edges['direction'] = 'both'
+
+oneway_yes = edges['oneway'] == 'yes'
+oneway_rev = edges['oneway'] == '-1'
+oneway_missing = edges['oneway'].isna() | edges['oneway'].isin(['None', ''])
+
+is_motorway = edges['highway'].isin(['motorway', 'motorway_link'])
+is_roundabout = edges['junction'].isin(['roundabout'])
+
+edges.loc[oneway_yes, 'direction'] = 'forward'
+edges.loc[oneway_rev, 'direction'] = 'backward'
+default_oneway = (is_motorway | is_roundabout) & oneway_missing
+edges.loc[default_oneway, 'direction'] = 'forward'
+
+# Forward edges (forward or bidirectional)
+forward_edges = edges[edges['direction'].isin(['forward', 'both'])].copy()
+forward_edges['source_id'] = forward_edges['u'].map(id_map)
+forward_edges['target_id'] = forward_edges['v'].map(id_map)
+forward_edges.rename(columns={'length': 'distance'}, inplace=True)
+forward_edges = forward_edges[['source_id', 'target_id', 'distance']]
+
+# Backward edges (backward or bidirectional)
+backward_edges = edges[edges['direction'].isin(['backward', 'both'])].copy()
+backward_edges['source_id'] = backward_edges['v'].map(id_map)
+backward_edges['target_id'] = backward_edges['u'].map(id_map)
+backward_edges.rename(columns={'length': 'distance'}, inplace=True)
+backward_edges = backward_edges[['source_id', 'target_id', 'distance']]
+
+# Combine and save
+edges_out = pd.concat([forward_edges, backward_edges], ignore_index=True)
+edges_out.to_csv("data/edges.csv", index=False)
 
 print("done")
 print(f"Total Nodes: {len(nodes_out)}")
